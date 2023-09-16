6e672941b9a8bc806a976d065658ad70961f029f
Mr Groot <113226362+MrGrootx@users.noreply.github.com>
2023-09-16 13:48:20 +0530

    clear + dm Command Added ✅

---
+++ src/maincommands/Moderation/dm.js
@@ -0,0 +1,68 @@
+const { SlashCommandBuilder, EmbedBuilder, PermissionsBitField } = require("discord.js");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("dm")
+    .setDescription("Make me DM a user")
+    .addUserOption((option) =>
+      option.setName("user")
+      .setDescription("The user to DM")
+      .setRequired(true)
+    )
+    .addStringOption((option) =>
+      option
+        .setName("message")
+        .setDescription("The message to DM")
+        .setRequired(true)
+    ),
+
+  async execute(interaction) {
+
+    if(!interaction.member.permissions.has(PermissionsBitField.Flags.ManageMessages)) 
+    return await interaction.reply({
+      content: "You don't have permissions to run this command",
+      ephemeral: true,
+    })
+
+    const { chennel, client, options } = interaction;
+
+    const user = options.getMember("user");
+    const message = options.getString("message");
+
+    if (user.id === process.env.BOT_ID) {
+      return await interaction
+        .reply({
+          embeds: [
+            new EmbedBuilder()
+            .setColor('Red')
+            .setDescription('You cant DM me')
+          ]
+        })
+        .catch((err) => {});
+    }
+    user.send(message).catch(async (err) => {
+      console.log(err);
+
+      return await interaction
+        .editReply({
+          content: `Failed to send message, please try again`,
+        })
+        .catch((err) => {});
+    });
+
+    await interaction
+      .reply({
+        content: `**${message}** message sended **${user}**`,
+      })
+      .catch((err) => {});
+
+    await setTimeout(() => {
+      interaction.deleteReply();
+    }, 3500);
+  },
+};
+
+
+/**  
+ *Coded By : Mr Groot#9862
+*/
